933b1a5461829d2e069464dfb39662093497f054
TrungDT <thanhtrung.it25@gmail.com>
2017-09-20 10:20:21 +0700

    Step 14: Refactor App into Home and setup react-router

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,7 @@ import GifList from '../components/GifList';
 import GifModal from '../components/GifModal';
 import '../styles/app.css';
 
-class App extends React.Component {
+class Home extends React.Component {
     render() {
         return (
             <div>
@@ -36,4 +36,4 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
